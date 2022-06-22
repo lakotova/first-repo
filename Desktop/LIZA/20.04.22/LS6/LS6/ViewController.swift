f32c13a6194aa4347eb5ab46891d313c36d94d83
Robert Langdon <quatroFormaggiapp@gmail.com>
2022-06-22 21:44:14 +0300

    initial commit

---
+++ Desktop/LIZA/20.04.22/LS6/LS6/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LS6
+//
+//  Created by Vik Holovach on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
